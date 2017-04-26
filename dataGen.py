6a93105da79c6f266c103f6075e24986311d41a9
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-26 16:48:37 -0700

    now intersecting with a box and getting the list of elements that intersect it

--- dataGen.py
+++ dataGen.py
@@ -2,9 +2,27 @@
 import clr
 import math
  
-clr.AddReference('RevitAPI') 
+clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI') 
 from Autodesk.Revit.DB import *
 
 app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
+
+#filter = ElementCategoryFilter(BuiltInCategory.OST_Topography)
+#collector = FilteredElementCollector(doc)
+
+#elemList = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
+#print(elemList.Count)
+
+pt1 = XYZ(0,0,0)
+pt2 = XYZ(20,20,20)
+box = Outline(pt1, pt2)
+
+
+filter = BoundingBoxIntersectsFilter(box)
+collector = FilteredElementCollector(doc)
+
+elemList = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
+for i in range(elemList.Count):
+	print(i,elemList[i].Id.IntegerValue)
