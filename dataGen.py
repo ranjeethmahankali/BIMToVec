fbab6b635aac82136ad9cb4807f924f9b8d2183d
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-04-30 15:54:18 -0700

    looping and collecting data at random places in the mode

--- dataGen.py
+++ dataGen.py
@@ -7,6 +7,7 @@ be the reason for feet.
 #import libraries and reference the RevitAPI and RevitAPIUI
 import clr
 import math
+import random
  
 clr.AddReference('RevitAPI')
 clr.AddReference('RevitAPIUI')
@@ -15,35 +16,56 @@ from Autodesk.Revit.DB import *
 app = __revit__.Application
 doc = __revit__.ActiveUIDocument.Document
 
-#filter = ElementCategoryFilter(BuiltInCategory.OST_Topography)
-#collector = FilteredElementCollector(doc)
+#main code starts
+#make sure 3d view is active
+bigbox = doc.ActiveView.GetSectionBox()
+minPt = bigbox.Min
+maxPt = bigbox.Max
 
-#elemList = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
-#print(elemList.Count)
-
-pt1 = XYZ(0,0,0)
-pt2 = XYZ(20,20,20)
-box = Outline(pt1, pt2)
-
-filter = BoundingBoxIntersectsFilter(box)
-collector = FilteredElementCollector(doc)
-
-elemList = collector.WherePasses(filter).WhereElementIsNotElementType().ToElements()
-for i in range(elemList.Count):
-	if not elemList[i].Category is None:
+size = 1
+path = 'output.txt'
+f = open(path, 'w')
+f.truncate()
+for i in range(1000):
+	pt1 = XYZ(random.uniform(minPt.X,maxPt.X),
+				random.uniform(minPt.Y,maxPt.Y),
+				random.uniform(minPt.Z,maxPt.Z))
+	diagonal = XYZ(size,size,size)
+	pt2 = pt1.Add(diagonal)
+	box = Outline(pt1, pt2)
+	
+	filter = BoundingBoxIntersectsFilter(box)
+	notCamFilter = ElementCategoryFilter(BuiltInCategory.OST_Cameras, True)
+	notSketchFilter = ElementClassFilter(Sketch, True)
+	notTopoFilter = ElementCategoryFilter(BuiltInCategory.OST_Topography,True)
+	notLines = ElementClassFilter(CurveElement, True)
+	
+	collector = FilteredElementCollector(doc)
+	
+	collector = collector.WherePasses(filter).WherePasses(notCamFilter).WherePasses(notSketchFilter)
+	collector = collector.WherePasses(notTopoFilter).WherePasses(notLines)
+	collector = collector.WhereElementIsNotElementType().WhereElementIsViewIndependent()
+	elemList = collector.ToElements()
+	
+	if elemList.Count < 2:continue
+	#print(elemList.Count)
+	for i in range(elemList.Count):
+		if elemList[i].Category.Name == "<Sketch>":
+			print(elemList[i])
+		#if not elemList[i].Category is None
 		matList = elemList[i].GetMaterialIds(False)
 		matNames = []
 		for mat in matList:
-			matNames.append(doc.GetElement(mat).Name)
-		print(elemList[i].Category.Name, matNames)
-
-
+			matNames.append(doc.GetElement(mat).MaterialCategory)
+		f.write(elemList[i].Category.Name+"; M: "+",".join(matNames)+"\n")
+	f.write('-------------\n')
+f.close()
 
 """
 pt1 = XYZ(0,0,0)
-pt2 = XYZ(0,20,0)
-pt3 = XYZ(20,20,0)
-pt4 = XYZ(20,0,0)
+pt2 = XYZ(0,size,0)
+pt3 = XYZ(size,size,0)
+pt4 = XYZ(size,0,0)
 
 l1 = Line.CreateBound(pt1, pt2)
 l2 = Line.CreateBound(pt2, pt3)
@@ -52,7 +74,7 @@ l4 = Line.CreateBound(pt4, pt1)
 
 loop = CurveLoop.Create([l1,l2,l3,l4])
 
-box = GeometryCreationUtilities.CreateExtrusionGeometry([loop], XYZ(0,0,1), 20)
+box = GeometryCreationUtilities.CreateExtrusionGeometry([loop], XYZ(0,0,1), 25)
 
 t = Transaction(doc, "adding stuff")
 t.Start()
