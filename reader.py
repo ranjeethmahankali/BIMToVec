9dab3988ffd75ea6add089f3779c6fe2674b0103
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-09-29 16:28:23 -0700

    started the reader logic with ascii lookup

--- reader.py
+++ reader.py
@@ -1,1 +1,12 @@
 from ops import *
+
+# convert a character to ascii code (8 bits)
+def prepareChar(ch):
+    if len(ch) != 1:
+        raise ValueError("Please supply a single character for the ascii lookup")
+    binStr = "{0:08b}".format(ord(ch))
+    binList = []
+    for d in binStr:
+        binList.append(int(d))
+    
+    return binList
