a7f51a0130ce3b67bcba19da2d090866b5acc254
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-12 17:48:48 -0700

    re-thinking the whole reader strategy

--- ops.py
+++ ops.py
@@ -18,7 +18,7 @@ LOG_DIR = "train_log/"
 learning_rate = 1e-4
 beta = 0.1 #momentum
 # coefficient for l2_loss
-alpha = 0.001
+alpha = 0.0005
 
 model_save_path = 'savedEmbeddings/conv_reader.ckpt'
 
