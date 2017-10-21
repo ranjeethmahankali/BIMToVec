5dea4c6eec5f5377239abf1140c8a5010095ac8b
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-21 16:35:22 -0700

    more cycles..

--- train_reader.py
+++ train_reader.py
@@ -46,7 +46,7 @@ def next_batch(num):
         
 
 merged_summary = tf.summary.merge_all()
-steps = 24000
+steps = 48000
 logStep = steps//100
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
