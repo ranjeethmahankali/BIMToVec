47710c3ecd19c3051359f9afbaca2ebbe0abdbde
Ranjeeth Mahankali <ranjeethmahankali@gmail.com>
2017-10-24 14:53:17 -0700

    Added a new project for the revit plugin  for BIMToVec

---
+++ BIMToVecDotNet/RevitBIMToVec/RevitBIMToVec.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+
+namespace RevitBIMToVec
+{
+    public class RevitBIMToVec
+    {
+    }
+}
--- dataGen/CopyBIMToVecSampler.bat
+++
@@ -1,9 +0,0 @@
-﻿TITLE Copying files...
-set buildSource=D:\Works\myPrograms\BIMToVec\BIMToVecSampler\BIMToVecSampler\bin\Release
-set target=D:\Works\myPrograms\BIMToVec\dataGen\BIMToVecSampler
-
-RMDIR /S /Q %target%
-MKDIR %target%
-XCOPY %buildSource% %target% /S /E /Y
-ECHO All the files are copied
-TIMEOUT 3
